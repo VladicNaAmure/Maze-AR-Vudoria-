70d43e35ad3981b70ad13d411c1a199c93a0bd99
Vladislav Radchenko <56233697+VladicNaAmure@users.noreply.github.com>
2020-03-01 22:37:15 +0300

    Add files via upload

---
+++ Assets/Scripts/ControllerBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerBox : MonoBehaviour
+{
+    public AudioClip[] aMusic;
+    public AudioSource aReaction;
+    string aName;
+
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aReaction = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount>0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                aName = hit.transform.gameObject.tag;
+                switch (aName)
+                {
+                    case "Box":
+                        aReaction.clip = aMusic[index];
+                        aReaction.Play();
+                        break;
+                }
+            }
+            if (hit.collider.tag == "Box")
+            {
+                hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    public int level;
+    public void LoadLeve()
+    {
+        // load new scene of int value
+        SceneManager.LoadScene(level);
+    }
+    public void RestartLevel()
+    {
+        //restart
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
