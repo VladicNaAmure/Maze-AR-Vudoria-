22f697ac02d5c974d76020e2727b82ee91fa538e
Vladislav Radchenko <56233697+VladicNaAmure@users.noreply.github.com>
2020-03-01 22:37:55 +0300

    Add files via upload

---
+++ Assets/TestForBag/MouseActive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseActive : MonoBehaviour
+{
+    public int index;
+    public GameObject activeRat;
+    public GameObject[] destroyRats;
+
+    GameObject indexRats;
+
+    void OnMouseDown()
+    {
+        if (GameObject.Find("ChangeColor").GetComponent<TagChoose>().activeDestroy == true)
+        {
+            activeRat.SetActive(true);
+
+            for (int i = 0; i < index; i++)
+            {
+                indexRats = destroyRats[i];
+                Destroy(indexRats);
+            }
+        }
+        }
+
+    }
+
---
+++ Assets/TestForBag/MouseClicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            foreach (Touch t in Input.touches)
+            {
+                if (t.phase == TouchPhase.Began)
+                {
+                    Vector3 point = new Vector3(t.position.x, t.position.y, 0);
+                    Ray ray = Camera.main.ScreenPointToRay(point);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        hit.collider.SendMessage("OnMouseDown", null, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+        }
+    }
+}
