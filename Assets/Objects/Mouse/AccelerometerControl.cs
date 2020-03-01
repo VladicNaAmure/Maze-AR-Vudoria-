2cd8cae264d80dc4aef4a59dc0159d8f45d1502a
Vladislav Radchenko <56233697+VladicNaAmure@users.noreply.github.com>
2020-03-01 22:26:35 +0300

    Add files via upload

---
+++ Assets/Objects/Mouse/AccelerometerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerControl : MonoBehaviour
+{
+    public bool isBool = true;
+    private Rigidbody rigBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float x = Input.acceleration.x;
+        //float z = -Input.acceleration.z;
+
+        Vector3 tilt = Input.acceleration;
+
+        //if (isBool)
+            //tilt = Quaternion.Euler(0, 0, 0) * tilt;
+
+        rigBody.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }
+
+    public void IsBool()
+    {
+        isBool = !isBool;
+    }
+}
